b31df91ace6aed496b4e29737dbfd871b3a879b7
Bryson <bdecker7@byu.edu>
2025-02-04 23:15:15 -0700

    cleaned up some code/comments

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -49,10 +49,6 @@ public class ChessGame {
      * startPosition
      */
 
-    //If the king is not in check on this team, get valid moves
-    //If the king is not left in danger from moving this piece, return moves
-    //if piece is not null
-
 //    Takes as input a position on the chessboard and returns all moves the piece there can legally make.
 //    If there is no piece at that location, this method returns null. A move is valid if it is a "piece move" for
 //    the piece at the input location and making that move would not leave the team’s king in danger of check.
@@ -122,20 +118,16 @@ public class ChessGame {
      * @param move chess move to preform
      * @throws InvalidMoveException if move is invalid
      */
-    //A move is illegal if it is not a "valid" move for the piece at the starting location,
-    // or if it’s not the corresponding team's turn.
 
-//    Receives a given move and executes it, provided it is a legal move. If the move is illegal, it throws an InvalidMoveException.
+//    Receives a given move and executes it, provided it is a legal move.
+//    If the move is illegal, it throws an InvalidMoveException.
 //    A move is illegal if it is not a "valid" move for the piece at the starting location, or if it’s not the corresponding team's turn.
     public void makeMove(ChessMove move) throws InvalidMoveException {
 
-//        ArrayList<ChessMove> validMove = new ArrayList<>();
-//        validMove = validMoves(move.getStartPosition());
         if(validMoves(move.getStartPosition()) == null || !(gameBoard.getPiece(move.getStartPosition()).getTeamColor() == turn)){
             throw new InvalidMoveException("Invalid move!");}
 
         else if(validMoves(move.getStartPosition()) != null){
-         //   ChessPiece currentPiece = new ChessPiece(gameBoard.getPiece(move.getStartPosition()).getTeamColor(),gameBoard.getPiece(move.getStartPosition()).getPieceType());
             if(validMoves(move.getStartPosition()).contains(move)){
 
                 if(move.getPromotionPiece() != null){
@@ -165,13 +157,11 @@ public class ChessGame {
      */
 
     //Returns true if the specified team’s King could be captured by an opposing piece.
-
+    //iterate through all the opposite colored pieces.
+    // if the piece contains the same position as the king of current team color, return true
     public boolean isInCheck(TeamColor teamColor) {
-        //iterate through all the opposite colored pieces.
-        // if the piece contains the same position as the king of current team color, return true
         Collection<ChessMove> OtherTeamMoves = new ArrayList<ChessMove>();
         ChessPosition currentTeamsKingPosition = null;
-        ChessMove teamsKingMoves = null;
         for(int i = 1; i <= 8; i++){
             for(int j = 1; j <= 8; j++){
                 ChessPosition currentPosition = new ChessPosition(i,j);
@@ -179,7 +169,7 @@ public class ChessGame {
 
                 //grabs all the moves of the opposing side, and the position of the current kings team
                 if(currentGamePiece != null){
-                    if(currentGamePiece.getTeamColor() != teamColor){ //if it's not the same team, get collection of moves it can take
+                    if(currentGamePiece.getTeamColor() != teamColor){           //if it's not the same team, get collection of moves it can take
                         OtherTeamMoves.addAll(currentGamePiece.pieceMoves(gameBoard,currentPosition));
                     }
                     else if(currentGamePiece.getPieceType() == ChessPiece.PieceType.KING && (currentGamePiece.getTeamColor() == teamColor)){
@@ -230,7 +220,6 @@ public class ChessGame {
      */
     public boolean isInStalemate(TeamColor teamColor) {
         Collection<ChessMove> OutOfAllMoves = new ArrayList<ChessMove>();
-        ChessPosition currentTeamsKingPosition = null;
         for(int i = 1; i <= 8; i++) {
             for (int j = 1; j <= 8; j++) {
                 ChessPosition currentPosition = new ChessPosition(i, j);
