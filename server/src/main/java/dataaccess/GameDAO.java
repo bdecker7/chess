3838542184f05330caa432da6b13ac3a49732478
Bryson <bdecker7@byu.edu>
2025-02-18 15:36:31 -0700

    GameDAO interface

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,17 @@
 package dataaccess;
 
+import chess.ChessGame;
+
+import java.util.ArrayList;
+
+//createGame: Create a new game.
+//getGame: Retrieve a specified game with the given game ID.
+//listGames: Retrieve all games.
+//updateGame: Updates a chess game. It should replace the chess game string corresponding to a given gameID. This is used when players join a game or when a move is made.
 public interface GameDAO {
+    void createGame();
+    ChessGame getGame(int gameID);
+    ArrayList<ChessGame> listGames();
+    void updateGame(); //not sure if this returns anything
+
 }
