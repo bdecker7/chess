dbf6a8781e0b17071546f58f3fe849a79838e4cb
Bryson <bdecker7@byu.edu>
2025-02-18 15:32:39 -0700

    UserDAO interface

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,11 @@
 package dataaccess;
 
+//clear: A method for clearing all data from the database. This is used during testing.
+//createUser: Create a new user.
+//getUser: Retrieve a user with the given username.
+
 public interface UserDAO {
+    void clear();
+    void createUser();
+    void getUser();
 }
