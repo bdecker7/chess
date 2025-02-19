28b74428203ef4b6d3da95593e419a7f53c39f30
Bryson <bdecker7@byu.edu>
2025-02-18 18:41:22 -0700

    AuthDAO interface implementation

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,9 @@
 package dataaccess;
-
+//createAuth: Create a new authorization.
+//getAuth: Retrieve an authorization given an authToken.
+//deleteAuth: Delete an authorization so that it is no longer valid.
 public interface AuthDAO {
+    void createAuth();
+    String getAuth(String authToken);
+    void deleteAuth(String authToken);
 }
