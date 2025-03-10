de8e1eca897ac1bf36292c540dc873e8b4f4500d
Bryson <bdecker7@byu.edu>
2025-03-10 14:46:48 -0600

    game implements create in sql:

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -3,12 +3,13 @@ package dataaccess;
 import model.GameData;
 import chess.ChessGame;
 
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashMap;
 
 public interface GameDAO {
 
-    GameData createGame(String gameName);
+    GameData createGame(String gameName) throws SQLException;
     boolean checkIfGameExists(int gameID);
     GameData getGame(int gameID);
     Collection<GameData> listGames();
--- server/src/main/java/dataaccess/GameSQL.java
+++ server/src/main/java/dataaccess/GameSQL.java
@@ -1,16 +1,35 @@
 package dataaccess;
 
 import chess.ChessGame;
+import dataaccess.exceptions.DataAccessException;
+import dataaccess.exceptions.SQLERROR;
 import model.GameData;
 
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class GameSQL implements GameDAO{
+
+    public GameSQL() throws SQLException, DataAccessException {
+        configureDatabase();
+    }
+
     @Override
-    public GameData createGame(String gameName) {
-        return null;
+    public GameData createGame(String gameName) throws SQLException {
+        Random random = new Random();
+        int newGameID = 1000 + random.nextInt(9000);
+        ChessGame game = new ChessGame();
+        GameData newGameData = new GameData(newGameID,null,null,gameName,game);
+
+        var statement = "INSERT INTO gameData (gameID, whiteUsername, blackUsername, gameName, gameObject) VALUES (? , ? , ?, ?, ?)";
+        executeUpdate(statement,newGameID,newGameData.whiteUsername(),newGameData.blackUsername(),newGameData);
+        return newGameData;
     }
 
     @Override
@@ -41,5 +60,52 @@ public class GameSQL implements GameDAO{
     @Override
     public HashMap<Integer, GameData> getGameHash() {
         return null;
+    }
+
+    private void executeUpdate(String statement, Object... params) throws SQLException, SQLERROR {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+// I dont think I need this because we are not indexing.
+//                var rs = ps.getGeneratedKeys();
+//                if (rs.next()) {
+//                    return rs.getInt(1);
+//                }
+
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new SQLERROR(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  gameData (
+              `gameID` int(4) NOT NULL,
+              `whiteUsername` varchar(256) NULL,
+              `blackUsername` varchar(256) NULL,
+              `gameObject` varchar(256) NOT NULL,
+              PRIMARY KEY (`gameID`)
+            )
+            """
+    };
+
+
+    private void configureDatabase() throws SQLException, DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new SQLERROR(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 }
