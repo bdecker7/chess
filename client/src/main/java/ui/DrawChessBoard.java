50743ce62fbeb1b841bbf6f06b3cc2b42aaff3a3
Bryson <bdecker7@byu.edu>
2025-03-14 22:52:54 -0600

    drew headers for chessboard

---
+++ client/src/main/java/ui/DrawChessBoard.java
@@ -0,0 +1,46 @@
+package ui;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static ui.EscapeSequences.*;
+import static ui.EscapeSequences.SET_TEXT_COLOR_WHITE;
+
+public class DrawChessBoard {
+
+    private static final int HEADERLENGTH = 10;
+
+    public static void main(String[] args) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        out.print(ERASE_SCREEN);
+
+        drawHeaders(out);
+    }
+
+
+    private static void drawHeaders(PrintStream out) {
+
+        setBlack(out);
+
+        String[] headers = {"  "," a ", " b ", " c "," d "," e "," f "," g "," h ", "  " };
+        for (int boardCol = 0; boardCol < HEADERLENGTH ; ++boardCol) {
+            printHeaderText(out, headers[boardCol]);
+        }
+        out.println();
+    }
+
+    private static void printHeaderText(PrintStream out, String headerText) {
+        out.print(SET_BG_COLOR_DARK_GREEN);
+        out.print(SET_TEXT_COLOR_WHITE);
+
+        out.print(headerText);
+
+        setBlack(out);
+    }
+
+    private static void setBlack(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_BLACK);
+    }
+}
