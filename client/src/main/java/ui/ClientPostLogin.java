5a1873a84085ea7d4accd256a606482959d87d26
Bryson <bdecker7@byu.edu>
2025-03-20 11:41:42 -0600

    made menue for Client Post login

--- client/src/main/java/ui/ClientPostLogin.java
+++ client/src/main/java/ui/ClientPostLogin.java
@@ -1,4 +1,65 @@
 package ui;
 
+import java.util.Arrays;
+
 public class ClientPostLogin {
+
+    public ClientPostLogin(String serverUrl, Repl repl) {
+    }
+
+    public String evalPost(String input ) {
+
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "1" -> create_game(params);
+                case "2" -> list_game(params);
+                case "3" -> join_game(params);
+                case "4" -> play_game(params);
+                case "5" -> observe_game(params);
+                case "6" -> logout(params);
+
+                default -> help();
+            };
+        } catch (InvalidRequest ex) {
+            return ex.getMessage();
+        }
+    }
+
+    private String create_game(String[] params) {
+        //call serverfacade class
+        return "here_createGame";
+    }
+
+    private String list_game(String[] params) {
+        return "here_listGame";
+    }
+
+    private String join_game(String[] params){
+        return "here_joinGame";
+    }
+    private String play_game(String[] params){
+        return "here_playGame";
+    }
+    private String observe_game(String[] params){
+        return "here_observeGame";
+    }
+    private String logout(String[] params){
+        return "here_logout";
+    }
+
+    String help() {
+
+        return "Type the number you wish that coorelates with what you would like to do: \n \n" +
+                "1. Create Game\n" +
+                "2. List Games\n" +
+                "3. Join Game\n" +
+                "4. Play Game\n" +
+                "5. Observe Game\n" +
+                "6. Logout\n\n" +
+                "Request: ";
+    }
+
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 
 public class Repl {
 
-    private final ClientPreLogin client;
+    private final ClientPreLogin clientPreLogin;
+    private final ClientPostLogin clientPostLogin;
 
     public Repl(String serverUrl) {
-        client = new ClientPreLogin(serverUrl, this);
+        clientPreLogin = new ClientPreLogin(serverUrl, this);
+        clientPostLogin = new ClientPostLogin(serverUrl,this);
     }
 
     public void run() {
-        System.out.println("\uD83D\uDC36 Welcome to Bryson's Chess Server!! Type in a command");
-        System.out.print(client.help());
+        System.out.println("♘♜ Welcome to Bryson's Chess Server!! Type in a command");
+        System.out.print(clientPreLogin.help());
 
         Scanner scanner = new Scanner(System.in);
         var result = "";
+        var result2 = "";
         while (!result.equals("quit")) {
             printPrompt();
             String line = scanner.nextLine();
 
             try {
-                result = client.eval(line);
-                System.out.print(result);
+                result = clientPreLogin.eval(line);
+
+                System.out.print(result2);
             } catch (Throwable e) {
                 var msg = e.toString();
                 System.out.print(msg);
